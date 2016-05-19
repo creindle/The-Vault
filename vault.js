0389f77b35b9f999f962872498d8c0a2f3bea952
Christie R <creindle@hawaii.edu>
2016-05-18 18:09:57 -1000

    Trying to figure out different ways to do this :O

--- vault.js
+++ vault.js
@@ -1,4 +1,21 @@
 'use strict';
 module.exports = function() {
-  
+  //_underscore denotes private variable
+  var _secretValue = {};
+  function setValue(key, value) {
+    _secretValue[key] = value;
+    console.log(_secretValue);
+  }
+  function getValue(key) {
+    if (_secretValue.hasOwnProperty(key)) {
+      return _secretValue[key];
+    }
+    else {
+      return null;
+    }
+  }
+  return {
+    setValue: setValue,
+    getValue: getValue
+  };
 };
